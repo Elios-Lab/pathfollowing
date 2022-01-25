22d7e394811360b9a5f8e85ff56bb4bbc36f23bd
lucalazzaroni <lucalazzaroni95@gmail.com>
2022-01-25 13:36:45 +0100

    Update to the last version

--- Assets/Scripts/PathFollowing/PathFollowingAgent.cs
+++ Assets/Scripts/PathFollowing/PathFollowingAgent.cs
@@ -11,335 +11,173 @@ using Random = UnityEngine.Random;
 [RequireComponent(typeof(Rigidbody))]
 [RequireComponent(typeof(SimulationManagerPFollowing))]
 
+public class PathFollowingAgent : Agent {
 
-
-public class PathFollowingAgent : Agent
-{
-
-    StatsRecorder statsRecorder;
-
-    public enum RewardType
-    {
-        Goal,
-        Collision,
-        Dense,
-        TimeOut
-    }
-
-    //debug purpouse
-    private float printReward=0;
-
-    //Var to save the percentage of goals reached over the number of iterations
-    private float ratio = 0;
-
-    private Rigidbody _rigitBody;
+    private Rigidbody _rigidBody;
     private CarController _controller;
     private SimulationManagerPFollowing _simulationManager;
     private ActionSegment<float> _lastActions;
+    private GameObject _target; 
+
+    StatsRecorder recorder;
+
+    public enum RewardType { Goal, Collision, Dense, TimeOut }
 
     private bool isTraining;
     private int maxIteration;
+    private bool hasCollided;
 
-    //Numero di collisioni
-    private int colNumber = 0;
-    //Numero di goal 
-    private int goalNumber = 0;
-    //Numero di volte in cui l'episodio termina senza goal/collisione
-    private int timeoutNum = 0;
+    private int stat_period = 10;
+    private int episode_count = 0;
+    private int collisions_count = 0;
+    private int goals_achieved_count = 0;
+    private int timeouts_count = 0;
 
-    private GameObject _targetGoal;
+    private float dense_reward = 0;
+    private float time_reward = 0;
+    private float distance_reward = 0;
+    private float allignment_reward = 0;
+    private float collisions_reward = 0;
+    private float goals_reward = 0;
 
-    //Vector3 circular array for saving positions 
-    //private float[] arrayOfPosition = new float[20];
-
-    //Index to manage array datas
-    //private int bufferIndex = 0;
-
-    //arrayOfPosition variance
-    //private float arrayVariance;
-
-    //Variance threshold;
-    //public float varianceThreshold;
-
-    public void Awake()
-    {
-        statsRecorder = Academy.Instance.StatsRecorder;
-    }
-
-    public override void Initialize()
-    {
-        _rigitBody = GetComponent<Rigidbody>();
+    public override void Initialize() {
+        _rigidBody = GetComponent<Rigidbody>();
         _controller = GetComponent<CarController>();
         _simulationManager = GetComponent<SimulationManagerPFollowing>();
         isTraining = _simulationManager.configurationManager.isTraining;
         maxIteration = _simulationManager.configurationManager.maxIteration;
         _simulationManager.InitializeSimulation();
+        recorder = Academy.Instance.StatsRecorder;
+
+        if (isTraining == false) stat_period = maxIteration;
     }
 
-    public override void OnEpisodeBegin()
-    {
-        //Stop the simulation if isOver true
-        if(_simulationManager.configurationManager.isOver == true && isTraining == false)
-        {
-            Time.timeScale = 0;
-        }
-        
-        if(isTraining == false && _simulationManager.configurationManager.isOver == false)
-        {
-            _simulationManager.configurationManager.iteration++;
-        }
-
-        //Array.Clear(arrayOfPosition, 0, 20);
+    public override void OnEpisodeBegin() {
+        if(_simulationManager.configurationManager.isOver == true && isTraining == false) Time.timeScale = 0;
+        if(isTraining == false && _simulationManager.configurationManager.isOver == false) _simulationManager.configurationManager.iteration++;
         _simulationManager.InitializeSimulation();
-        _targetGoal = null;
+       _target = _simulationManager.configurationManager.goal;
+       hasCollided = false;
+        UpdateRatio();
     }
 
-    public override void OnActionReceived(ActionBuffers vectorAction)
-    {
+    public override void OnActionReceived(ActionBuffers vectorAction) {
         _lastActions = vectorAction.ContinuousActions;
         _controller.CurrentSteeringAngle = vectorAction.ContinuousActions[0];
         _controller.CurrentAcceleration = vectorAction.ContinuousActions[1];
         _controller.CurrentBrakeTorque = vectorAction.ContinuousActions[2];
     }
 
-    public override void Heuristic(in ActionBuffers actionsOut)
-    {
+    public override void Heuristic(in ActionBuffers actionsOut) {
         ActionSegment<float> continuousActionsOut = actionsOut.ContinuousActions;
         continuousActionsOut[0] = Input.GetAxis("Horizontal");
         continuousActionsOut[1] = Input.GetAxis("Vertical");
         continuousActionsOut[2] = Input.GetAxis("Jump");
     }
 
-    private void OnCollisionEnter(Collision other)
-    {
-        if (other.gameObject.CompareTag("barrier"))
-        {
+    // Timeout
+    void FixedUpdate() { if(StepCount == MaxStep - 1) TimeOut(); }
 
-            AddReward(ComputeReward(RewardType.Collision));
-            IncrementCollision();
+    // Collision
+    private void OnCollisionEnter(Collision other) {  if (other.gameObject.CompareTag("barrier")) CollisionReward(); }
 
-            EndEpisode();
-
-        }
-    }
-
-    private void FixedUpdate()
-    {
-        if (StepCount == MaxStep - 1)
-        {
-            IncrementTimeout();
-            ComputeReward(RewardType.TimeOut);
-        }
-
-        if (((goalNumber + timeoutNum + colNumber) % 100) == 0)
-        {
-            statsRecorder.Add("environment/Goal Ratio %", 100 * (float)goalNumber / (goalNumber + timeoutNum + colNumber));
-            statsRecorder.Add("environment/Collision Ratio %", 100 * (float)colNumber / (goalNumber + timeoutNum + colNumber));
-            statsRecorder.Add("environmen/Timeout Ratio %", 100 * (float)timeoutNum / (goalNumber + timeoutNum + colNumber));
-        }
-    }
-
-    public override void CollectObservations(VectorSensor sensor)
-    {
+    // Observation
+    public override void CollectObservations(VectorSensor sensor) {
+        if (!_simulationManager.InitComplete) { sensor.AddObservation(new float[9]); return; }
+        if (_simulationManager.configurationManager.iteration >= maxIteration && isTraining == false) _simulationManager.configurationManager.isOver = true;
         
-        //Check if we are in model checking mode
-        if (isTraining == false && colNumber + goalNumber + timeoutNum != 0)
-            //Update ratio
-           ratio = goalNumber / (float)(colNumber + goalNumber + timeoutNum);
+        // Observations
+        Vector2 agent_position = new Vector2(transform.position.x, transform.position.z);
+        Vector2 target_position = new Vector2(_target.transform.position.x, _target.transform.position.z);
+        Vector2 velocity = new Vector2(_rigidBody.velocity.x, _rigidBody.velocity.z);
+        Vector2 agent_forward = new Vector2(transform.forward.x, transform.forward.z);
+        Vector2 target_forward = new Vector2(_target.transform.forward.x, _target.transform.forward.z);
+        int time = StepCount;
 
-        if (_simulationManager.configurationManager.iteration >= maxIteration && isTraining == false)
-        {
-            _simulationManager.configurationManager.isOver = true;
-        }
+        sensor.AddObservation(target_position - agent_position); // Direction towards the target
+        sensor.AddObservation(velocity); // Agent velocity
+        sensor.AddObservation(agent_forward); // Agent direction
+        sensor.AddObservation(target_forward); // Target direction
+        //sensor.AddObservation(time); // Time
 
-        if (_simulationManager.InitComplete)
-        {
-            //Check the index value, set it to 0 if has reached the array's max size
-            //if(bufferIndex > 19)
-            //{
-            //    bufferIndex = 0;
-            //}
+        // Add dense reward
+        float distance = Mathf.Abs(_target.transform.position.x - transform.position.x) + Mathf.Abs(_target.transform.position.z - transform.position.z); 
+        float alignment = Mathf.Abs(Vector3.Dot(agent_forward, target_forward));
 
-            if (_targetGoal == null)
-                _targetGoal = _simulationManager.configurationManager.goal;
-            //_targetGoal = _simulationManager.target[i];
-            Vector3 distance = _targetGoal.transform.position - transform.position;
-            Vector3 dirToTarget = (distance).normalized;
-            float magDistance = distance.magnitude;
-            Vector3 agentPos = transform.position - _simulationManager.configurationManager.environment.transform.position;
-            //Vector3 targetPos = _targetGoal.transform.position - _simulationManager.configurationManager.environment.transform.position;
-
-            //arrayOfPosition[bufferIndex] = agentPos.magnitude;
-            /*
-            if(arrayOfPosition[19] != 0f)
-            {
-                //Compute variance
-                StartCoroutine(ComputeVariance(arrayOfPosition, 20));
-            }*/ 
-
-            //Observations
-            sensor.AddObservation(agentPos.normalized);
-            sensor.AddObservation(this.transform.InverseTransformPoint(_targetGoal.transform.position));
-            sensor.AddObservation(
-                this.transform.InverseTransformVector(_rigitBody.velocity.normalized)); //come sono allinato con la velocità
-            sensor.AddObservation(
-                this.transform.InverseTransformDirection(dirToTarget)); //dove dovrei andare
-            //valore di riferimento per obs precedenti.
-            sensor.AddObservation(transform.forward);
-            sensor.AddObservation(transform.right);
-            //sensor.AddObservation(StepCount / MaxStep);
-            float velocityAlignment = Vector3.Dot(dirToTarget, _rigitBody.velocity.normalized);
-            
-            //Dense reward 
-            AddReward(ComputeReward(RewardType.Dense, velocityAlignment, magDistance));
-
-            //Increase buffer index 
-            //bufferIndex++;
-        }
-        else
-        {
-            sensor.AddObservation(new float[18]);
-        }
+        DenseReward(alignment, time, distance);
     }
 
-    public IEnumerator JackpotReward(float bonus)
-    {
+    public void UpdateRatio() {
+        if(episode_count >= stat_period) {
+            recorder.Add("Events/Goals", 100 * goals_achieved_count / (float)stat_period);
+            recorder.Add("Events/Timeouts", 100 * timeouts_count / (float)stat_period);
+            recorder.Add("Events/Collisions", 100 * collisions_count / (float)(stat_period));
+            recorder.Add("Rewards/Goals", goals_reward / (float)stat_period);
+            recorder.Add("Rewards/Dense", dense_reward / (float)stat_period);
+            recorder.Add("Rewards/Collisions", collisions_reward / (float)stat_period);
+            recorder.Add("Rewards/Time", time_reward / (float)stat_period);
+            recorder.Add("Rewards/Distance", distance_reward / (float)stat_period);
+            recorder.Add("Rewards/Allignment", allignment_reward / (float)stat_period);
+            time_reward = 0;
+            distance_reward = 0;
+            allignment_reward = 0;
+            episode_count=0;
+            goals_achieved_count = 0;
+            timeouts_count = 0;
+            collisions_count = 0;
+            dense_reward = 0;
+            collisions_reward = 0;
+            goals_reward = 0;
+        }
+        if (isTraining == false) print("Episode: " + episode_count + " Goals: " + goals_achieved_count + " Collisions: " + collisions_count + " Timeouts: " + timeouts_count);
+        episode_count++;
+    }
 
-        AddReward(ComputeReward(RewardType.Goal, bonus));
-        IncrementGoal();
+    // Dense reward
+    private float sigmoid(float value) { return (float)(1.0f / (1.0f + Math.Pow(Math.E, -value))); }
+    private float distance_contribution(float weight, float distance) { return (float)(-weight * distance); }
+    private float alignment_contribution(float weight, float alignment, float distance) { return (float)(-weight * (1 - alignment) / (distance * distance + 1)); }
+    private float time_contribution(float weight, float time, float distance) { return -weight * sigmoid((time/30)-4) * sigmoid(distance-4); }
+    public void DenseReward(float alignment, float time, float distance) {
+        //float tc = time_contribution(0.1f, time, distance);
+        float tc = 0;
+        float dc = distance_contribution(0.01f, distance);
+        float ac = alignment_contribution(1.5f, alignment, distance);
+        float reward = 0.01f * ( tc + dc + ac );
+        //Debug.Log("dc: " + dc + " ac: " + ac + " tc: " + tc + "reward: " + reward);
+        if (isTraining == true) AddReward(reward);
+        dense_reward += reward;
+        time_reward += tc;
+        distance_reward += dc;
+        allignment_reward += ac;
+    }
 
-        yield return new WaitForEndOfFrame();
-
+    // Goal reward
+    public void GoalReward() {
+        float reward = 1.0f;
+        //Debug.Log(reward);
+        if (isTraining == true) AddReward(reward);
+        goals_reward += reward;
+        goals_achieved_count++;
         EndEpisode();
-
     }
 
-    public float ComputeReward(RewardType type, float bonus1 = 0f, float bonus2 = 0f)
-    {
-
-        ///////REWARD TABLE
-        /// Ending alignment (bonus1 for goal) goes from: fermandosi in modo coerente con l'allineamento max 0.8 min 0.3, contromano max 0.2 min 0
-        /// Dot product (bonus1 for dense) goes from -1 to 1
-        /// Distance from target (bonus2) is the distance to target. Considering a 100m^2 is 100*sqrt(2). Min distance is 0
-
-        ///////REWARD RANGES
-        /// Collision: -75
-        /// DENSE
-        /// Goal: from 40 to 100
-
-        //reward var
-        float reward = 0;
-
-        ////////// Reward values
-        float rewardGoal = 50f; //was 40
-        float rewardCollision = -100f; //backup 75
-        //float rewardStucked = -50f;
-
-        //coefficient for the ending alignment between the agent and the goal
-        float c0 = 75f;
-        //Angle
-        float c1 = 1f;
-        //Coeff. distance
-        float c2 = -0.01f;
-        //Coeff to scale relu-like function
-        float c3 = 0.01f;
-
-
-        //float c3 = 0.001f;
-
-        switch (type)
-        {
-            case RewardType.Goal:
-                //calcolo reward goal             
-                //il goal fornisce all'agente un reward pari a 0.3 + il valore bonus dato dall'allineamento moltiplicato per il coeff c0
-                reward = rewardGoal + c0 * bonus1;
-                //Debug.Log("Goal reached! Reward: " + reward + " Steps " + StepCount);
-                break;
-            case RewardType.Dense:
-                //calcolo reward denso, dato dal prodottto tra una costante e l'allineamento della velocità rispetto al target
-                reward = c1 * (bonus1 - 1) / (bonus2 + 1) + c2 * (bonus2 * bonus2) * Mathf.Max(1, StepCount - 700) * c3; //-1 aggiunto al bonus per avere reward sempre negativo tranne quando sta facendo bene |||| aggiungere distanza 
-                this.printReward = reward;
-                break;
-            case RewardType.Collision:
-                //penalità per aver colpito un ostacolo
-                reward = rewardCollision;
-                //Debug.Log("Collision detected! Reward: " + reward + " Steps " + StepCount);
-                break;
-            /*
-            case RewardType.Stuck:
-                reward = rewardStucked;
-                //Debug.Log("Stucked Vehicle");
-                break;*/
+    // Collision reward
+    public void CollisionReward() {
+        float reward = -0.5f;
+        //Debug.Log(reward);
+        if (isTraining == true) AddReward(reward);
+        collisions_reward += reward;
+        if(!hasCollided) {
+            collisions_count++;
+            hasCollided = true;
         }
-
-        return reward;
+        EndEpisode();
     }
 
-    public void IncrementCollision()
-    {
-        colNumber++;
-
-        if (isTraining == false) 
-        { 
-            Debug.Log("Collision number: " + colNumber);
-            Debug.Log("Success rate: " + ratio);
-        }
+    // Timeout
+     public void TimeOut() {
+        timeouts_count++;
+        EndEpisode();
     }
-
-    public void IncrementGoal()
-    {
-        goalNumber++;
-
-        if (isTraining == false) 
-        { 
-            Debug.Log("Goal number: " + goalNumber);
-            Debug.Log("Success rate: " + ratio);
-        }
-    }
-
-    public void IncrementTimeout()
-    {
-        timeoutNum++;
-
-        if(isTraining == false) 
-        { 
-            Debug.Log("Timeouts: " + timeoutNum);
-            Debug.Log("Success rate: " + ratio);
-        }
-    }
-
-    
-    // variance
-    /*
-    public IEnumerator ComputeVariance(float[] a, int n)
-    {
-        // Compute mean (average of elements)
-        float sum = 0f;
-
-        for (int i = 0; i < n; i++)
-            sum += a[i];
-
-        float mean = (float)sum / (float)n;
-
-        // Compute sum squared
-        // differences with mean.
-        float sqDiff = 0f;
-
-        for (int i = 0; i < n; i++)
-            sqDiff += (a[i] - mean) * (a[i] - mean);
-
-        arrayVariance = sqDiff / n;
-        //Debug.Log("Variance: " + arrayVariance);
-
-        if (arrayVariance < varianceThreshold)
-        {
-            //Debug.LogWarning("Threshold");
-            AddReward(ComputeReward(RewardType.Stuck));
-
-            yield return new WaitForEndOfFrame();
-
-            EndEpisode();
-        }
-    }*/
-
 }
