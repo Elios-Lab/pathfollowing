ab2035b2c8bf68a99666ea112941c49a0ed2c056
lucalazzaroni <lucalazzaroni95@gmail.com>
2022-05-18 18:28:18 +0200

    Parsing of env params from local JSON implemented

--- Assets/Scripts/PathFollowing/ConfigurationManager.cs
+++ Assets/Scripts/PathFollowing/ConfigurationManager.cs
@@ -2,22 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Collections;
 using UnityEngine;
+using System.IO;
 
 public class ConfigurationManager : MonoBehaviour
 {
-
     [SerializeField] public PathFollowingAgent agent;
     [SerializeField] public GameObject environment;
     [SerializeField] public GameObject goal;
-    [SerializeField] public float carLength;
-    [SerializeField] public float carWidth;
+    [SerializeField] public ConfigDeserializer parameters;
+    [SerializeField] public GameObject carModel;
+    // [SerializeField] public float carLength;
+    // [SerializeField] public float carWidth;
 
     //Variable to set the configuration in training mode or model checking mode
     public bool isTraining;
-    
+
     //Declaring a variable for the number of iteration we wants to check the model
     public int iteration;
-    public int maxIteration; 
+    public int maxIteration;
 
     //Garage transform list
     [SerializeField] public Transform[] garage;
@@ -25,11 +27,38 @@ public class ConfigurationManager : MonoBehaviour
     //Flag that becomes true when the number of iteration has reached maxIteration
     public bool isOver = false;
 
+    [System.Serializable]
+    public class ConfigDeserializer
+    {
+        public Car car;
+    }
 
+    [System.Serializable]
+    public class Car
+    {
+        public float length;
+        public float width;
+        public float height;
+        public float weight;
+        public float steering;
+        public float torque;
+        public float brake;
+
+    }
+
+    private Vector3 carSize = new Vector3(1.788832f, 4.705657f, 0.9729889f);
+    private Vector3 carScale = new Vector3(100, 100, 100);
+
+    private void Awake()
+    {
+        parameters = JsonUtility.FromJson<ConfigDeserializer>(ReadJSON());
+        ScaleCar();
+        SetCarParameters();
+    }
 
     void Update()
     {
-   
+
     }
 
     /*
@@ -39,6 +68,28 @@ public class ConfigurationManager : MonoBehaviour
         GUI.Label(new Rect(10,10,100,100),"Ratio= " + ratio.ToString("F"));
     }*/
 
+    void ScaleCar()
+    // Scale the car and its collider according to the JSON file specifics
+    {
+        // Proportion is carSize : carScale = newSize : newScale
+        Vector3 newScale = new Vector3(
+            carScale.x * parameters.car.width / carSize.x,
+            carScale.y * parameters.car.length / carSize.y,
+            carScale.z * parameters.car.height / carSize.z);
+
+        carModel.GetComponent<BoxCollider>().size = new Vector3(parameters.car.width / newScale.x, parameters.car.length / newScale.y, parameters.car.height / newScale.z);
+        carModel.transform.localScale = newScale;
+        return;
+    }
+
+    void SetCarParameters()
+    // Set the car's parameters according to the JSON file specifics
+    {
+        agent.GetComponent<Rigidbody>().mass = parameters.car.weight;
+        agent.GetComponent<CarController>().maxSteeringAngle = parameters.car.steering;
+        agent.GetComponent<CarController>().maxMotorTorque = parameters.car.torque;
+        agent.GetComponent<CarController>().maxBrakeTorque = parameters.car.brake;
+    }
 
     public void RandomAgentPositioning()
     {
@@ -47,13 +98,13 @@ public class ConfigurationManager : MonoBehaviour
             //Call the function for the generation of the random spot position
             float maxX = 0, maxZ = 0, minX = 0, minZ = 0;
 
-            float rotation = Random.Range(0,360);
+            float rotation = Random.Range(0, 360);
 
             float x_base;
             float z_base;
 
-            maxX = 9.75f- (carWidth / 2) * Mathf.Abs(Mathf.Cos(Mathf.Deg2Rad * rotation)) - (carLength / 2) * Mathf.Abs(Mathf.Sin(Mathf.Deg2Rad * rotation));
-            maxZ = 3.75f - (carWidth / 2) * Mathf.Abs(Mathf.Sin(Mathf.Deg2Rad * rotation)) - (carLength / 2) * Mathf.Abs(Mathf.Cos(Mathf.Deg2Rad * rotation));
+            maxX = 9.75f - (parameters.car.width / 2) * Mathf.Abs(Mathf.Cos(Mathf.Deg2Rad * rotation)) - (parameters.car.length / 2) * Mathf.Abs(Mathf.Sin(Mathf.Deg2Rad * rotation));
+            maxZ = 3.75f - (parameters.car.width / 2) * Mathf.Abs(Mathf.Sin(Mathf.Deg2Rad * rotation)) - (parameters.car.length / 2) * Mathf.Abs(Mathf.Cos(Mathf.Deg2Rad * rotation));
             minX = -maxX;
             minZ = -maxZ;
 
@@ -93,20 +144,20 @@ public class ConfigurationManager : MonoBehaviour
         //int i = 5;
 
         //Set the goal rotation -90 garage above 90 garage below
-        if (i >= 0 && i <= 4)  
+        if (i >= 0 && i <= 4)
         {
             goal.transform.rotation = Quaternion.Euler(0, 90, 0);
         }
         else
         {
             goal.transform.rotation = Quaternion.Euler(0, -90, 0);
-        }   
+        }
 
         //Set the goal position to the TargetSpawn(child object of garage) position
         goal.transform.position = garage[i].GetChild(4).position;
 
         //Opens the randomly chosen garage and close the others
-        for(int j = 0; j < garage.Length; j++)
+        for (int j = 0; j < garage.Length; j++)
         {
             Vector3 position = garage[j].GetChild(3).position;
             Quaternion rotation = garage[j].GetChild(3).rotation;
@@ -123,4 +174,13 @@ public class ConfigurationManager : MonoBehaviour
 
     }
 
+    public static string ReadJSON()
+    {
+        string path = "./config/env_config.json";
+        //Read the text from directly from the file
+        StreamReader reader = new StreamReader(path);
+        string text = reader.ReadToEnd();
+        reader.Close();
+        return text;
+    }
 }
--- README.md
+++ README.md
@@ -2,56 +2,65 @@
 
 ## Prerequisites:
 
-* Unity: >= 2020.3.22f1
-* Python3: >= 3.8.10
-* MLagents: 0.25.1 
-* MLagents package version on Unity: 2.0.0
+* Unity: >= 2020.3.22f1 (tested on 2021.3.2f1)
+* Python: >= 3.8.10
+* ML-Agents: >= 0.25.1 (tested on 0.29.0)
+* ML-Agents Unity package version: 2.0.0
 
 To install all the requirements use the command:
 
-<code>pip install -r /path/to/requirements.txt</code>
+```pip install mlagents```
 
-## Links:
+or, alternatively:
+
+```pip install -r requirements.txt```
+
+## Useful links:
 
 * [Unity](https://unity.com/download)
 * [Unity documentation](https://docs.unity3d.com/Manual/index.html)
-* [MLAgents](https://github.com/Unity-Technologies/ml-agents)
+* [ML-Agents](https://github.com/Unity-Technologies/ml-agents)
 
-Some of the 3D models used in this project are from Sketchfab:
+The cars 3D models used in this project come from Sketchfab:
 
-* [Shrine of The Fury](https://sketchfab.com/3d-models/chevrolet-corvette-1980-different-colours-7e428bdb3ab54b4e9ac610e545fd9d03)
+* [Chevrolet Corvette 1980 Different colours](https://sketchfab.com/3d-models/chevrolet-corvette-1980-different-colours-7e428bdb3ab54b4e9ac610e545fd9d03)
 
 # How to open the project in Unity?
 
 After cloning or downloading the repository, you have to follow these simple steps to open the project and work on it:
 
 1. Open the Unity Hub 
-2. In Projects click ADD 
-3. Select the folder in which has been saved and open it
+2. Open an existing project
+3. Select the folder in which the project has been saved and open it
 
 Note that a Unity project is a collection of files and directories, rather than just one specific Unity Project file. To open a Project, you must select the main Project folder, rather than a specific file.
 
-After opening the project you need to install the MLAgents package vers.2.0.0 from Window\Package Manager in unity editor.
+After opening the project you need to install the ML-Agents package vers. 2.0.0 from Window/Package Manager in unity editor.
 
 # ML-Agents setup
 
 ## Installation
 
-* [Installation](https://github.com/Unity-Technologies/ml-agents/blob/release_18_docs/docs/Installation.md)
+* [Installation](https://github.com/Unity-Technologies/ml-agents/blob/main/docs/Installation.md)
 
 ## Documentation
 
 For further information it is better to consult the official documentation:
-* [Documentation](https://github.com/Unity-Technologies/ml-agents/blob/release_18_docs/docs/Readme.md) 
+* [Documentation](https://github.com/Unity-Technologies/ml-agents/blob/main/docs/Readme.md) 
 
 Here is a detailed description of all the features [ML-Agents Overview](https://github.com/Unity-Technologies/ml-agents/blob/main/docs/ML-Agents-Overview.md). 
 
 
 ## Training 
+Training can be performed in the Unity editor or in a built project.
+To train inside the editor use the following command:
 
-Go in the project folder and use the following command:
+```mlagents-learn config/trainer_config.yaml --run-id=name```
 
-<code>mlagents-learn config/trainer_config.yaml --run-id=name</code>
+ Otherwise, to train using a built project:
 
+ ```mlagents-learn config/trainer_config.yaml --run-id=name --env=path/to/built/project```
+
+Note that if a run id is already present, training will not start. To overwrite an existing run id add ```--force``` to the above command.
 For more informations and features:
-* [Training Documentation](https://github.com/Unity-Technologies/ml-agents/blob/release_18_docs/docs/Training-ML-Agents.md)
+* [Training Documentation](https://github.com/Unity-Technologies/ml-agents/blob/main/docs/Training-ML-Agents.md)
